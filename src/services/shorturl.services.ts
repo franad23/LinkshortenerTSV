0ed821ca42bf5a9be663d52acd3736e1d05a2ada
Franco Diaz <dfrancoandres@gmail.com>
2023-12-12 20:46:51 -0300

    se corrige url de api

--- src/services/shorturl.services.ts
+++ src/services/shorturl.services.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const shortUrl = async (originalUrl: string, continueUrl:boolean) => {
-  return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/shorturl", {
+  return await axios.post("https://linkshortener-tsv-backend.vercel.app/api/shorturl", {
     originalUrl,
     continueUrl
   },{
@@ -10,13 +10,13 @@ export const shortUrl = async (originalUrl: string, continueUrl:boolean) => {
 }
 
 export const shortUrlCounter = async () => {
-  return await axios.get("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/urlshortcount",{
+  return await axios.get("https://linkshortener-tsv-backend.vercel.app/api/urlshortcount",{
     withCredentials: true 
   })
 }
 
 export const verifyUrl = async (shortedUrl: string) => {
-  return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/verify", {
+  return await axios.post("https://linkshortener-tsv-backend.vercel.app/api/verify", {
     shortedUrl
   },{
     withCredentials: true 
