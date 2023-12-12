3324fd6f816b8c57182d8d5887adce3f977d97e9
Franco Diaz <dfrancoandres@gmail.com>
2023-12-12 20:35:53 -0300

    se corrige url del backend

--- src/services/shorturl.services.ts
+++ src/services/shorturl.services.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export const shortUrl = async (originalUrl: string, continueUrl:boolean) => {
-  return await axios.post("http://localhost:3000/api/shorturl", {
+  return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/shorturl", {
     originalUrl,
     continueUrl
   })
 }
 
 export const shortUrlCounter = async () => {
-  return await axios.get("http://localhost:3000/api/urlshortcount")
+  return await axios.get("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/urlshortcount")
 }
 
 export const verifyUrl = async (shortedUrl: string) => {
-  return await axios.post("http://localhost:3000/api/verify", {
+  return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/verify", {
     shortedUrl
   })
 }
