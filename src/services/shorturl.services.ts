e88f39d1a44f1c623f84d5f1fc5955d07227492d
Franco Diaz <dfrancoandres@gmail.com>
2023-12-12 20:44:26 -0300

    se agregan credenciales de axios

--- src/services/shorturl.services.ts
+++ src/services/shorturl.services.ts
@@ -4,15 +4,21 @@ export const shortUrl = async (originalUrl: string, continueUrl:boolean) => {
   return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/shorturl", {
     originalUrl,
     continueUrl
+  },{
+    withCredentials: true 
   })
 }
 
 export const shortUrlCounter = async () => {
-  return await axios.get("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/urlshortcount")
+  return await axios.get("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/urlshortcount",{
+    withCredentials: true 
+  })
 }
 
 export const verifyUrl = async (shortedUrl: string) => {
   return await axios.post("https://linkshortener-tsv-backend-5v3blx7gb-franad23.vercel.app/api/verify", {
     shortedUrl
+  },{
+    withCredentials: true 
   })
 }
